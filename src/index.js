7665b9d5182d1545c964ad5895ee593de6df46a6
jpdreamthug <lentorilp@gmail.com>
2021-03-11 20:08:08 +0200

    init: start typical-array-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (arguments.lenght==0 || array.length == 0) ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (arguments.legth == 0 || array.length == 0) ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (arguments.length == 0 || array.length == 0) ? 0 : array.reduce((current, sum) => sum += current) / array.length;
 }
